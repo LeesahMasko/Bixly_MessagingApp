fb585c67d56160ef0490792cedc45a6a0f924c80
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-09-01 14:10:25 -0700

    header style mostly completed

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import MenuRoundedIcon from '@material-ui/icons/MenuRounded';
+import AccountCircleRoundedIcon from '@material-ui/icons/AccountCircleRounded';
 
 
 function Header() {
@@ -8,6 +10,11 @@ function Header() {
             <CatLogoWrapper>
                 <img src="/Cat.png" alt="Cat Chat logo, 2 cat outlines with text in between" width= "200" height="75"></img>
             </CatLogoWrapper>
+            <ProfileIconWrapper>
+                <MenuRoundedIcon />
+                <AccountCircleRoundedIcon />
+
+            </ProfileIconWrapper>
         </Wrapper>
 
     )
@@ -16,6 +23,11 @@ function Header() {
 export default Header
 
 const Wrapper = styled.div`
-
+display: flex;
+justify-content: space-between;
+align-items: flex-end;
+border-bottom: 4px solid #9da8a8
 `
 const CatLogoWrapper = styled.div``
+
+const ProfileIconWrapper = styled.div ``
