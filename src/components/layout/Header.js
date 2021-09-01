22d642ebabf4e0beef18d31834be5c1b60fe273c
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2021-09-01 13:37:08 -0700

    adding the logo and styled components

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
+
 
 function Header() {
     return (
-        <div>Header</div>
+        <Wrapper>
+            <CatLogoWrapper>
+                <img src="/Cat.png" alt="Cat Chat logo, 2 cat outlines with text in between" width= "200" height="75"></img>
+            </CatLogoWrapper>
+        </Wrapper>
+
     )
 }
 
 export default Header
 
+const Wrapper = styled.div`
+
+`
+const CatLogoWrapper = styled.div``
